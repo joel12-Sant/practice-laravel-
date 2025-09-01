549dfcb5299359d7c0ef0a5c57ea605dc8d8ce21
joel <joelsantiagos000@gmail.com>
2025-09-01 15:37:49 -0600

    finciona brezze laravel

--- laravel/vite.config.js
+++ laravel/vite.config.js
@@ -2,10 +2,14 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
-    plugins: [
-        laravel({
-            input: ['resources/scss/app.scss', 'resources/js/app.js'],
-            refresh: true,
-        }),
-    ],
+  plugins: [
+    laravel({
+      input: [
+        'resources/css/app.css',   // Tailwind
+        'resources/scss/app.scss', // SCSS
+        'resources/js/app.js',
+      ],
+      refresh: true,
+    }),
+  ],
 });
